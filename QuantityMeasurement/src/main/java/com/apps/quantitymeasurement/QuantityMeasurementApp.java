97b573977740ef2f20572eb80152b191090d6e5e
KU20406298 <kundan.kumar73@wipro.com>
2026-01-30 15:43:01 +0530

    UC2 code Feet Measurement equality added

--- QuantityMeasurement/src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ QuantityMeasurement/src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -23,14 +23,25 @@ public class QuantityMeasurementApp {
             this.value1 = value1;
             this.value2 = value2;
 
+            if(value1 == null || value2 == null) {
+                throw new NullPointerException("Null Value not allowed");
+            }
             if (Double.isNaN(value1) || Double.isInfinite(value1) && Double.isNaN(value2) || Double.isInfinite(value2)) {
                 throw new IllegalArgumentException("Invalid double value");
             }
-            if (value1 < 0 || value1 > 13 && value2 < 0 || value2 > 13) { // Example range check
-                throw new IllegalArgumentException("Value out of range");
-            }
         }
 
+        //Define a static method to demonstrate Feet equality check
+        public static void demonstrateFeetEquaility(Double value1, Double value2) {
+            boolean flag = false;
+            QuantityMeasurementApp.Feet feet = new QuantityMeasurementApp.Feet(value1,value2);
+            if(value1 == null || value2 == null) {
+                throw new NullPointerException("Null Value not allowed");
+            }
+             flag = Double.compare(value1, feet.value1) == 0 && Double.compare(value2, feet.value2) == 0;
+            System.out.println("Feet Equality :: "+ flag);
+
+        }
         //override equals method for equality check
 
         @Override
@@ -53,15 +64,66 @@ public class QuantityMeasurementApp {
         }
 
     }
+    public static class Inches {
 
-    public static void main(String[] args) {
+        private final Double value1;
+        private final Double value2;
 
-       System.out.println("Feet measurement");
-       Feet feet = new Feet(Double.parseDouble("ddd"),10.1);
-      if(feet.value1 !=null && feet.value2 != null) {
-        boolean flag =  feet.value1.equals(feet.value2);
-          System.out.println(flag);
-      }
+        public Inches(Double value1, Double value2) {
+
+            this.value1 = value1;
+            this.value2 = value2;
+
+            if (Double.isNaN(value1) || Double.isInfinite(value1) && Double.isNaN(value2) || Double.isInfinite(value2)) {
+                throw new IllegalArgumentException("Invalid double value");
+            }
+        }
+
+        public Double getValue1() {
+            return value1;
+        }
+
+        public Double getValue2() {
+            return value2;
+        }
+
+        //Define a static method to demonstrate Feet equality check
+        public static void demonstrateInchesEquaility(Double value1, Double value2) {
+            QuantityMeasurementApp.Inches inches = new QuantityMeasurementApp.Inches(value1, value2);
+            boolean flag = Double.compare(value1, inches.value1) == 0 && Double.compare(value2, inches.value2) == 0;
+            System.out.println("Inches Equality :: " + flag);
+
+        }
     }
+        public static void main(String[] args) {
+            boolean flag = false;
+            System.out.println("Feet measurement");
 
+            QuantityMeasurementApp.Feet feet = new QuantityMeasurementApp.Feet(10.1, 10.1);
+            Feet.demonstrateFeetEquaility(feet.value1, feet.value2);
+
+            QuantityMeasurementApp.Inches inches = new QuantityMeasurementApp.Inches(25.0, 25.0);
+            Inches.demonstrateInchesEquaility(inches.value1, inches.value2);
+
+        /*if(feet.value1 !=null && feet.value2 != null) {
+            flag =  feet.value1.equals(feet.value2);
+            System.out.println(flag);
+        }*/
+
+            //Different class
+            //QuantityMeasurementApp.Feet feetStrigVal = new QuantityMeasurementApp.Feet(Double.parseDouble("ddd"),10.1);
+            //flag =  feetStrigVal.value1.equals(feetStrigVal.value2);
+            //System.out.println(flag);
+
+            //null value
+            //QuantityMeasurementApp.Feet feetNullValue = new QuantityMeasurementApp.Feet(null,10.1);
+            //  flag = feetNullValue.value1.equals(feetNullValue.value2);
+            //System.out.println(flag);
+
+            QuantityMeasurementApp.Feet feetRange = new QuantityMeasurementApp.Feet(14.5, 14.5);
+            if (feetRange.value1 != null && feetRange.value2 != null) {
+                flag = feetRange.value1.equals(feetRange.value2);
+                System.out.println(flag);
+            }
+        }
 }
--- QuantityMeasurement/src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
+++ QuantityMeasurement/src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -5,17 +5,6 @@ import org.junit.jupiter.api.Test;
 
 public class QuantityMeasurementAppTest {
 
-    public static class Inch {
-
-        private final Double value1;
-        private final Double value2;
-
-        public Inch(Double value1, Double value2) {
-            this.value1 = value1;
-            this.value2 = value2;
-        }
-    }
-
     @Test
     public void testFeetEquality_SameValue(){
 
@@ -30,28 +19,61 @@ public class QuantityMeasurementAppTest {
         Assertions.assertNotEquals(feet.getValue1(),feet.getValue2());
     }
 
-    @Test
+    /*@Test
     public void testFeetEquality_NullComparision(){
-
         QuantityMeasurementApp.Feet feet = new QuantityMeasurementApp.Feet(null,20.1);
-        Assertions.assertNull(feet.getValue1());
-    }
+        Assertions.assertThrows(NullPointerException.class, () -> {
+            QuantityMeasurementApp.Feet.demonstrateFeetEquaility(feet.getValue1(),feet.getValue2());
+        });
+    }*/
 
     @Test
     public void testFeetEquality_DifferentClass(){
 
         QuantityMeasurementApp.Feet feet = new QuantityMeasurementApp.Feet(10.1,20.1);
-        Inch inch = new Inch(16.1,18.1);
-        Assertions.assertNotEquals(feet.getValue1(),inch.value1);
-        Assertions.assertNotEquals(feet.getValue2(),inch.value2);
+        QuantityMeasurementApp.Inches inches = new QuantityMeasurementApp.Inches(16.1,18.1);
+        Assertions.assertNotEquals(feet.getValue1(),inches.getValue1());
+        Assertions.assertNotEquals(feet.getValue2(),inches.getValue2());
     }
 
     @Test
     public void testFeetEquality_SameReference(){
-        QuantityMeasurementApp.Feet feet = new QuantityMeasurementApp.Feet(10.1,20.1);
+        QuantityMeasurementApp.Feet feet = new QuantityMeasurementApp.Feet(10.1,10.1);
         QuantityMeasurementApp.Feet feet2 = feet;
         Assertions.assertEquals(feet.getValue1(),feet2.getValue1());
         Assertions.assertEquals(feet.getValue2(),feet2.getValue2());
 
     }
+    @Test
+    public void testInchedEquality_SameValue(){
+
+        QuantityMeasurementApp.Inches inches = new QuantityMeasurementApp.Inches(10.1,20.1);
+        Assertions.assertNotEquals(inches.getValue1(),inches.getValue2());
+    }
+
+    @Test
+    public void testInchesEquality_DifferentValue(){
+
+        QuantityMeasurementApp.Inches inches = new QuantityMeasurementApp.Inches(10.1,20.1);
+        Assertions.assertNotEquals(inches.getValue1(),inches.getValue2());
+    }
+
+    @Test
+    public void testInchesEquality_DifferentClass(){
+
+        QuantityMeasurementApp.Inches inches = new QuantityMeasurementApp.Inches(10.1,20.1);
+        QuantityMeasurementApp.Feet feet = new QuantityMeasurementApp.Feet(16.1,18.1);
+        Assertions.assertNotEquals(inches.getValue1(),feet.getValue1());
+        Assertions.assertNotEquals(inches.getValue2(),feet.getValue2());
+    }
+
+    @Test
+    public void testInchesEquality_SameReference(){
+        QuantityMeasurementApp.Inches inches = new QuantityMeasurementApp.Inches(10.1,10.1);
+        QuantityMeasurementApp.Inches inches1 = inches;
+        Assertions.assertEquals(inches.getValue1(),inches1.getValue1());
+        Assertions.assertEquals(inches.getValue2(),inches1.getValue2());
+
+    }
+
 }
